325a5fb616df01941bb934fff881295c6b18fc65
Noah Jutz <noah@noahjutz.com>
2025-10-25 12:45:52 +0200

    Fix weird characters after first 0 byte, reset buffer every time

--- ue3-1b/src/Receiver.java
+++ ue3-1b/src/Receiver.java
@@ -1,11 +1,14 @@
 final int PORT = 6868;
+
 void main() {
     var buffer = new byte[100];
     try (var socket = new DatagramSocket(PORT)) {
         while (true) {
             try {
-                socket.receive(new DatagramPacket(buffer, buffer.length));
-                IO.println(new String(buffer, StandardCharsets.UTF_8));
+                final var packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                IO.println(new String(Arrays.copyOfRange(buffer, 0, packet.getLength()), StandardCharsets.UTF_8));
+                Arrays.fill(buffer, (byte) 0);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
