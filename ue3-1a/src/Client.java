640259412684f9f929aac98e84bb005f405f5a8d
Noah Jutz <noah@noahjutz.com>
2025-10-25 11:17:19 +0200

    Implement Client

--- ue3-1a/src/Client.java
+++ ue3-1a/src/Client.java
@@ -1,2 +1,19 @@
 void main(String[] args) {
+    assert args.length < 3;
+    var port = Args.parsePort(args);
+    var host = Args.parseAddr(args);
+
+    var scanner = new Scanner(System.in);
+    try {
+        var socket = new Socket(host, port);
+        var writer = new PrintWriter(socket.getOutputStream());
+        var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        while (true) {
+            var line = scanner.nextLine();
+            writer.println(line);
+            writer.flush();
+        }
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
 }
