d20b33a54174f4b31161a0b556eaa37a84f00f24
Noah Jutz <noah@noahjutz.com>
2025-10-25 10:56:46 +0200

    Fix file name, add reader/writer

--- ue3-1a/src/Server.java
+++ ue3-1a/src/Server.java
@@ -9,7 +9,17 @@ void main(String[] args) throws UnknownHostException {
     };
     var host = args.length == 2 ? InetAddress.getByName(args[0]) : InetAddress.getLocalHost();
     try (var serverSocket = new ServerSocket(port, 1, host)) {
+        while (true) {
+            var socket = serverSocket.accept();
+            var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            var writer = new PrintWriter(socket.getOutputStream());
 
+            writer.println("Hallo"); // oder print(("Hallo\n");
+            writer.flush();
+
+            String antwort = reader.readLine();
+            System.out.println("Antwort der Gegenseite: " + antwort);
+        }
     } catch (IOException e) {
         throw new RuntimeException(e);
     }
