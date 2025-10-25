b138fe9bfa2332247853bc362cd27f47f9e0356f
Noah Jutz <noah@noahjutz.com>
2025-10-25 11:19:25 +0200

    Print received messages to stdout from Client

--- ue3-1a/src/Client.java
+++ ue3-1a/src/Client.java
@@ -8,6 +8,16 @@ void main(String[] args) {
         var socket = new Socket(host, port);
         var writer = new PrintWriter(socket.getOutputStream());
         var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        new Thread(() -> {
+            while (true) {
+                try {
+                    var line = reader.readLine();
+                    IO.println(line);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
         while (true) {
             var line = scanner.nextLine();
             writer.println(line);
