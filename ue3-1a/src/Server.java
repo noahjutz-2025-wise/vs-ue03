93bbf85e9477610de65d63abb042816349179d4d
Noah Jutz <noah@noahjutz.com>
2025-10-25 11:25:51 +0200

    Make server compact source file again

--- ue3-1a/src/Server.java
+++ ue3-1a/src/Server.java
@@ -1,42 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.UnknownHostException;
+void main(String[] args) {
+    assert args.length < 3;
+    var port = Args.parsePort(args);
+    var host = Args.parseAddr(args);
+    try (var serverSocket = new ServerSocket(port, 1, host)) {
+        while (true) {
+            var socket = serverSocket.accept();
+            var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            var writer = new PrintWriter(socket.getOutputStream());
 
-public class Server {
-    public static void main(String[] args) {
-        assert args.length < 3;
-        var port = Args.parsePort(args);
-        var host = Args.parseAddr(args);
-        try (var serverSocket = new ServerSocket(port, 1, host)) {
-            while (true) {
-                var socket = serverSocket.accept();
-                var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                var writer = new PrintWriter(socket.getOutputStream());
-
-                new Thread(() -> {
-                    while (true) {
-                        try {
-                            var line = reader.readLine();
-                            writer.println(line.chars().map(c -> c + 1)
-                                    .collect(
-                                            StringBuilder::new,
-                                            (sb, i) -> sb.append((char) i),
-                                            StringBuilder::append
-                                    )
-                                    .toString());
-                            writer.flush();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        var line = reader.readLine();
+                        writer.println(line.chars().map(c -> c + 1)
+                                .collect(
+                                        StringBuilder::new,
+                                        (sb, i) -> sb.append((char) i),
+                                        StringBuilder::append
+                                )
+                                .toString());
+                        writer.flush();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
                     }
-                }).start();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                }
+            }).start();
         }
+    } catch (IOException e) {
+        throw new RuntimeException(e);
     }
 }
